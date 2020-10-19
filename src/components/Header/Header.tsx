309fd34a2c0610f4e96ce2a0acc9cde9b8bf531e
christovito <54348204+christovito@users.noreply.github.com>
2020-10-20 02:00:47 +0700

    Update Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,18 +3,18 @@ import styles from './Header.module.scss'
 
 const Header = () : ReactElement => {
   return (
-    <div className={styles.container}>
-      <div className={styles.title}>
-        UTS Teknologi Sistem Terintegrasi II3160
-      </div>
-      <div className={styles.title}>
-        Tafia Alifianty Dinita Putri
-      </div>
-      <div className={styles.title}>
-        18218038
-      </div>
-    </div>
-  )
+  //  <div className={styles.container}>
+  //    <div className={styles.title}>
+  //      UTS Teknologi Sistem Terintegrasi II3160
+  //    </div>
+  //    <div className={styles.title}>
+  //      Tafia Alifianty Dinita Putri
+  //    </div>
+  //    <div className={styles.title}>
+  //      18218038
+  //    </div>
+  //  </div>
+  //)
 }
 
 export default Header
